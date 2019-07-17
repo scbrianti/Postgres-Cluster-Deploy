f746f293750d9e2a5d8b53d85eb19b06b01acf66
Sidnei Brianti <scbrianti@gmail.com>
2019-07-17 17:28:54 -0300

    Update 01-master-install.sh

--- 01-master-install.sh
+++ 01-master-install.sh
@@ -13,11 +13,11 @@
 
 ODOO_DB_USER="odoo"
 ODOO_DB_PASS="odoo"
-MASTER_IP="192.168.1.10"
-SLAVE_IP="192.168.1.11"
-SLAVE_USER="sammy"
-SLAVE_PASS="sammy"
-NETWORK="192.168.1.0/24"
+MASTER="master.domain"
+SLAVE="slave.domain"
+SLAVE_USER="sidnei"
+SLAVE_PASS="sidnei"
+NETWORK="x.x.x.0/24"
 
 #--------------------------------------------------
 # Update Server
@@ -30,7 +30,7 @@ sudo apt upgrade -yV
 # Install PostgreSQL Server
 #--------------------------------------------------
 echo -e "\n---- Install PostgreSQL Server ----"
-sudo apt install postgresql-9.5 postgresql-9.5-repmgr postgresql-client-9.5 sshpass -yV
+sudo apt install postgresql-9.6 postgresql-9.6-repmgr postgresql-client-9.6 sshpass -yV
 
 echo -e "\n---- Configure password-less SSH ----"
 sudo su - postgres -c "ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
@@ -38,8 +38,8 @@ sudo su - postgres -c "ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
 echo -e "\n---- Copy RSA keys to Node2 ----"
 sudo su - postgres -c "cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys"
 sudo su - postgres -c "chmod go-rwx ~/.ssh/*"
-sudo su - postgres -c "ssh-keyscan -H $SLAVE_IP >> ~/.ssh/known_hosts"
-sudo su - postgres -c "sshpass -p $SLAVE_PASS scp ~/.ssh/id_rsa.pub ~/.ssh/id_rsa ~/.ssh/authorized_keys $SLAVE_USER@$SLAVE_IP:"
+sudo su - postgres -c "ssh-keyscan -H $SLAVE >> ~/.ssh/known_hosts"
+sudo su - postgres -c "sshpass -p $SLAVE_PASS scp ~/.ssh/id_rsa.pub ~/.ssh/id_rsa ~/.ssh/authorized_keys $SLAVE_USER@$SLAVE:"
 
 echo -e "\n---- Configure repmgr User & Database ----"
 sudo su - postgres -c "createuser -s repmgr" 2> /dev/null || true
@@ -50,19 +50,19 @@ sudo su - postgres -c "createuser -s $ODOO_DB_USER" 2> /dev/null || true
 sudo su - postgres -c "psql -c \"ALTER USER $ODOO_DB_USER WITH PASSWORD '$ODOO_DB_PASS';\""
 
 echo -e "\n---- Configure PostgreSQL Replication Settings ----"
-sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
-sudo sed -i "s/#wal_level = minimal/wal_level = hot_standby/g" /etc/postgresql/9.5/main/postgresql.conf
-sudo sed -i "s/#archive_mode = off/archive_mode = on/g" /etc/postgresql/9.5/main/postgresql.conf
-sudo sed -i "s/#archive_command = ''/archive_command = 'cd .'/g" /etc/postgresql/9.5/main/postgresql.conf
-sudo sed -i "s/#max_wal_senders = 0/max_wal_senders = 10/g" /etc/postgresql/9.5/main/postgresql.conf
-sudo sed -i "s/#max_replication_slots = 0/max_replication_slots = 1/g" /etc/postgresql/9.5/main/postgresql.conf
-sudo sed -i "s/#shared_preload_libraries = ''/shared_preload_libraries = 'repmgr_funcs'/g" /etc/postgresql/9.5/main/postgresql.conf
-sudo sed -i "s/#hot_standby = off/hot_standby = on/g" /etc/postgresql/9.5/main/postgresql.conf
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.6/main/postgresql.conf
+sudo sed -i "s/#wal_level = minimal/wal_level = hot_standby/g" /etc/postgresql/9.6/main/postgresql.conf
+sudo sed -i "s/#archive_mode = off/archive_mode = on/g" /etc/postgresql/9.6/main/postgresql.conf
+sudo sed -i "s/#archive_command = ''/archive_command = 'cd .'/g" /etc/postgresql/9.6/main/postgresql.conf
+sudo sed -i "s/#max_wal_senders = 0/max_wal_senders = 10/g" /etc/postgresql/9.6/main/postgresql.conf
+sudo sed -i "s/#max_replication_slots = 0/max_replication_slots = 1/g" /etc/postgresql/9.6/main/postgresql.conf
+sudo sed -i "s/#shared_preload_libraries = ''/shared_preload_libraries = 'repmgr_funcs'/g" /etc/postgresql/9.6/main/postgresql.conf
+sudo sed -i "s/#hot_standby = off/hot_standby = on/g" /etc/postgresql/9.6/main/postgresql.conf
 
 echo -e "\n---- Configure PostgreSQL Settings ----"
 echo "host    repmgr          repmgr     $NETWORK     trust
 host    replication     repmgr     $NETWORK     trust
-host    all     all     $NETWORK     trust" | sudo tee -a /etc/postgresql/9.5/main/pg_hba.conf
+host    all     all     $NETWORK     trust" | sudo tee -a /etc/postgresql/9.6/main/pg_hba.conf
 
 echo -e "\n---- Restart PostgreSQL Service ----"
 sudo service postgresql restart
@@ -73,8 +73,8 @@ echo "cluster=Odoo
 node=1
 node_name=node1
 use_replication_slots=1
-conninfo='host=$MASTER_IP user=repmgr dbname=repmgr'
-pg_bindir=/usr/lib/postgresql/9.5/bin" | sudo tee -a /etc/repmgr/repmgr.conf
+conninfo='host=$MASTER user=repmgr dbname=repmgr'
+pg_bindir=/usr/lib/postgresql/9.6/bin" | sudo tee -a /etc/repmgr/repmgr.conf
 sudo su - postgres -c "repmgr -f /etc/repmgr/repmgr.conf master register"
 
 echo -e "\n---- Prepare Failover Scripts ----"
@@ -85,7 +85,7 @@ EOF
 cat <<EOF > ~/demote-server
 #!/bin/bash
 sudo service postgresql stop
-sudo su - postgres -c "repmgr -f /etc/repmgr/repmgr.conf --force --rsync-only -h $SLAVE_IP -d repmgr -U repmgr --verbose standby clone"
+sudo su - postgres -c "repmgr -f /etc/repmgr/repmgr.conf --force --rsync-only -h $SLAVE -d repmgr -U repmgr --verbose standby clone"
 sudo service postgresql restart
 sudo su - postgres -c "repmgr -f /etc/repmgr/repmgr.conf --force standby register"
 EOF
